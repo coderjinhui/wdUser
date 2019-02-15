48a1f88425503e5717f03be45bc97bdb252734ba
HuangJin <coderjinhui@gmail.com>
2019-02-15 22:40:28 +0800

    start user api

--- backend/routes/user/user.route.js
+++ backend/routes/user/user.route.js
@@ -4,8 +4,28 @@ const user = new Router();
 const userService = require('./user.service');
 const routerUtil = require('../../utils/route.glue');
 
-user.prefix('/user');
+// get /api/user 获取全部用户
 
+// post /api/user 创建用户
+/* 参数
+ ** name: string
+ ** phone: string
+ ** costList?: ObjectID 对应消费表数据
+ **
+ */
+// 返回值 msg: success, data: 新创建的分类数据
+
+// delete /api/category/:id 删除分类
+
+// post /api/category/:id 修改分类
+// 参数 name: string
+// 返回值 data: 修改后的分类
+
+// get /api/category/search?keyword=11
+// 参数 keyword: string
+// 返回值 进行模糊搜索的结果数组
+
+user.prefix('/user');
 routerUtil.routeGlue(userService, user);
 
 module.exports = user;
--- backend/routes/user/user.service.js
+++ backend/routes/user/user.service.js
@@ -1,5 +1,24 @@
 const userModel = require('../../models/user');
 
+// get /api/user 获取全部商品列表
+async function getAllGoods(ctx) {
+  try {
+    let goods = await goodsModel.find({}).populate('category', '_id name');
+    // console.log('goods data', goods);
+    goods = simpleData(goods);
+
+    ctx.body = {
+      code: 0,
+      data: goods
+    };
+  } catch (err) {
+    ctx.body = {
+      code: 1,
+      msg: JSON.stringify(err)
+    };
+  }
+}
+
 module.exports = [
   // {url: '/', method: 'get', handler: getAll},
   // {url: '/add', method: 'post', handler: add},
